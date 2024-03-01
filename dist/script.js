2d9c2768bfc619a6efc3e27b2ac7a5b3b3d85182
Shree Jaybhay <shreejaybhay26@gmail.com>
2024-02-29 19:23:44 -0500

    update

--- dist/script.js
+++ dist/script.js
@@ -19,3 +19,54 @@ closemenu.addEventListener("click", () => {
 });
 
 // mobile navbar end
+
+
+//faq
+
+function onbtn() {
+  document.getElementById("pdiv").style.display = "block";
+  document.getElementById("plusbtn").style.display = "none";
+  document.getElementById("minusbtn").style.display = "block";
+}
+
+function ofbtn() {
+  document.getElementById("pdiv").style.display = "none";
+  document.getElementById("plusbtn").style.display = "block";
+  document.getElementById("minusbtn").style.display = "none";
+}
+
+function onbtn1() {
+  document.getElementById("pdiv1").style.display = "block";
+  document.getElementById("plusbtn1").style.display = "none";
+  document.getElementById("minusbtn1").style.display = "block";
+}
+
+function ofbtn1() {
+  document.getElementById("pdiv1").style.display = "none";
+  document.getElementById("plusbtn1").style.display = "block";
+  document.getElementById("minusbtn1").style.display = "none";
+}
+
+function onbtn2() {
+  document.getElementById("pdiv2").style.display = "block";
+  document.getElementById("plusbtn2").style.display = "none";
+  document.getElementById("minusbtn2").style.display = "block";
+}
+
+function ofbtn2() {
+  document.getElementById("pdiv2").style.display = "none";
+  document.getElementById("plusbtn2").style.display = "block";
+  document.getElementById("minusbtn2").style.display = "none";
+}
+
+function onbtn3() {
+  document.getElementById("pdiv3").style.display = "block";
+  document.getElementById("plusbtn3").style.display = "none";
+  document.getElementById("minusbtn3").style.display = "block";
+}
+
+function ofbtn3() {
+  document.getElementById("pdiv3").style.display = "none";
+  document.getElementById("plusbtn3").style.display = "block";
+  document.getElementById("minusbtn3").style.display = "none";
+}
